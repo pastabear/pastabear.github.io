899a3ff4d15f8017730191d1c95652fefa4266bd
pastabear <98814529+pastabear@users.noreply.github.com>
2022-02-07 11:38:24 +0100

    tries out sketch by BUN

--- sketch.js
+++ sketch.js
@@ -1,73 +1,67 @@
-//p5.js shader basic structure ref from https://www.openprocessing.org/sketch/920144
-let colors = "132a13-31572c-4f772d-90a955-ecf39e-f4b860-c83e4d".split("-").map(a => "#" + a)
-let chord = "C2,Eb2,G2,C3,Eb3,G3,C4,Eb4,G4,C5,Eb5,G5".split(",")
-// let chordSet = "A,E,A,E,G#".split(";").map(a=>a.split(","))
-let polySynth
-let theShader;
-
-let webglgraphics, graphics, graphics2;
-function playSynth(note='A3',dur) {
-  userStartAudio();
-  let vel = random(0.2);
-  polySynth.play(note, vel, 0, dur); 
-}
-function preload() {
-	theShader = new p5.Shader(this.renderer, vert, frag)
-}
-
-function setup() {
-	createCanvas(1000, 1000);
-	pixelDensity(2)
-	webglgraphics = createGraphics(1000, 1000, WEBGL);
-	graphics = createGraphics(width, height)
-	graphics2 = createGraphics(width, height)
-  polySynth = new p5.PolySynth();
-	polySynth.setADSR(1,1,1,1)
-	noStroke()
-	background(0);
- 	useChord = chord 
-	chord = [2,3,4,5].map(n=>useChord.map(note=>note+n)).flat().filter(a=>a)
-}
-
-let autoMovemouse = true
-
-function mouseMoved() {
-	autoMovemouse = false
-}
-let notePlayCount = 0 
-
-function draw() {
-	if (autoMovemouse) {
-		mouseX = sin(frameCount / 30) * width
-		mouseY = cos(frameCount / 40 + PI / 4) * height
-	}
-	if (frameCount%100==1){
-			let targetNode1 = chord[int(random(0,chord.length/2))]
-			let targetNode2 = chord[int(random(chord.length/2,chord.length))]
-			playSynth(targetNode1,random(5))
-			playSynth(targetNode2,random(5))
-			notePlayCount+=2
-		}
-	webglgraphics.shader(theShader)
-	theShader.setUniform('u_resolution', [width / 1000, height / 1000])
-	theShader.setUniform('u_time', millis() / 1000)
-	theShader.setUniform('u_mouse', [mouseX / width, mouseY / height])
-	theShader.setUniform('u_original_tex', graphics);
-	webglgraphics.rect(0, 0, width, height)
-
-	graphics.background(0, 100)
-	graphics.noStroke()
-	graphics.push()
-
-	image(webglgraphics, 0, 0)
-	image(graphics2, 0, 0)
-
-
-	stroke(255)
-}
-
-function keyPressed() {
-	if (key == ' ') {
-		save()
-	}
-}
+/**
+ * Added on October 22, 2020
+ * 
+ * Hi, I'm BUN.
+ * I've been surprised at how many people have forked over this sketch, more than I expected. Thanks.
+ * 
+ * Here is a sketch that explains this code with multiple codes.
+ * Please use it as a reference.
+ * https://www.openprocessing.org/sketch/793375
+ * 
+ * Brief explanation.
+ * This code primarily uses "Hook's Law".
+ * In other words, it simulates the motion of a spring.
+ * 
+ * From the mouse position, the position to be drawn is moved like a spring.
+ * And the width of the line is changed according to the speed of the movement.
+ * 
+ ** Added on May 8, 2021 **
+ * Gorilla Sun has written an explanation of each part with animations on his blog. Thank you!
+ * https://gorillasun.de/blog/Simulating-brush-strokes-with-Hooke's-Law-in-P5JS-and-Processing
+ **/
+ function setup() {
+    createCanvas(windowWidth,windowHeight);
+    distance = 10;
+    spring = 0.5;
+    friction = 0.5;
+    size = 25;
+    diff = size/8;
+    x = y = ax = ay = a = r = f = 0;
+  }
+  
+  function draw() {
+    oldR = r;
+    if(mouseIsPressed) {
+      mX = mouseX;
+      mY = mouseY;
+      if(!f) {
+        f = 1;
+        x = mX;
+        y = mY;
+      }
+      ax += ( mX - x ) * spring;
+      ay += ( mY - y ) * spring;
+      ax *= friction;
+      ay *= friction;
+      a += sqrt( ax*ax + ay*ay ) - a;
+      a *= 0.6;
+      r = size - a;
+      
+      for( i = 0; i < distance; ++i ) {
+        oldX = x;
+        oldY = y;
+        x += ax / distance;
+        y += ay / distance;
+        oldR += ( r - oldR ) / distance;
+        if(oldR < 1) oldR = 1;
+        strokeWeight( oldR+diff );
+        line( x, y, oldX, oldY );
+        strokeWeight( oldR );
+        line( x+diff*2, y+diff*2, oldX+diff*2, oldY+diff*2 );
+        line( x-diff, y-diff, oldX-diff, oldY-diff );
+      }
+    } else if(f) {
+      ax = ay = f = 0;
+    }
+  }
+  /**/
